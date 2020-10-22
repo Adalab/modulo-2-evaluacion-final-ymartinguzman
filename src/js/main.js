1e628817b6c2b49f3a749fe3c9e7aaadd31a341a
ymartinguzman <yamiramartinguzman@gmail.com>
2020-10-22 21:02:49 +0200

    declared const in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('>> Ready :)');
+const input = document.querySelector('.js-input');
+const btn = document.querySelector('.js-btn');
+const result = document.querySelector('.js-result');
+
+let inputValue = input.nodeValue;
+fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`);
+  .then((response) => response.json())
+  .then((data) => {
+
+})
