7e3f50e7894fb63d32140d7f38309ed2a48e21be
ymartinguzman <yamiramartinguzman@gmail.com>
2020-10-22 21:28:33 +0200

    add listener to getInfo function

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,19 @@
 const input = document.querySelector('.js-input');
 const btn = document.querySelector('.js-btn');
 const result = document.querySelector('.js-result');
+//data.show.name
+//data.score.image
 
-let inputValue = input.nodeValue;
-fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`);
-  .then((response) => response.json())
-  .then((data) => {
+function getInfo() {
+  let inputValue = input.value;
+  // console.log(inputValue);
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`);
+    .then((response) => response.json())
+    .then((data) => {
+    for (const list of data) {
+      // console.log(list);
+    }
+  });
+}
 
-})
+btn.addEventListener("click", getInfo);
