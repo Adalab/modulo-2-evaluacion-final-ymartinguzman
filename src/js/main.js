23c69ed9e7b0e2126cdfec0d4248df92f7937d7f
ymartinguzman <yamiramartinguzman@gmail.com>
2020-10-23 09:53:06 +0200

    add fetch to get infor from API

--- src/js/main.js
+++ src/js/main.js
@@ -6,16 +6,22 @@ const result = document.querySelector('.js-result');
 //data.show.name
 //data.score.image
 
+let listSeries = [];
 function getInfo() {
   let inputValue = input.value;
-  // console.log(inputValue);
-  fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`);
+  console.log(inputValue);
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
-    for (const list of data) {
-      // console.log(list);
-    }
-  });
+      for (const list of data) {
+        console.log(list);
+        result.innerHTML += `<li>`;
+        result.innerHTML += `<img src = ${list.show.image}
+        alt = picture serie shown >`;
+        result.innerHTML += `<h2> ${list.show.name} </h2>`;
+        result.innerHTML += `</li>`;
+      }
+    });
 }
 
-btn.addEventListener("click", getInfo);
+btn.addEventListener('click', getInfo);
