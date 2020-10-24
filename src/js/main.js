9ee07817ee04ff45041f7f0000dc2d3a819bdc09
ymartinguzman <yamiramartinguzman@gmail.com>
2020-10-24 18:45:39 +0200

    add favourites series to list and show

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 const input = document.querySelector('.js-input');
 const btn = document.querySelector('.js-btn');
 const result = document.querySelector('.js-result');
+const favList = document.querySelector('.js-result-fav');
 //data.show.name
 //data.score.image
 
@@ -10,21 +11,24 @@ let listSeries = [];
 let favouritesList = [];
 
 function getInfo() {
+  //obtener info de la api, tarda!
   let inputValue = input.value;
   fetch(`//api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
       listSeries = data;
     });
+    
 
   paintSeries();
   listenList();
+  paintFavList();
 }
 
 function paintSeries() {
   let resultsHtml = '';
   for (let i = 0; i < listSeries.length; i++) {
-    resultsHtml += `<li  class="js-list-item " id="${i}">`;
+    resultsHtml += `<li  class="js-list-item js-list-color " id="${i}">`;
     resultsHtml += `<h2> ${listSeries[i].show.name} </h2>`;
     if (listSeries[i].show.image === null) {
       resultsHtml += `<img src = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV."`;
@@ -39,38 +43,27 @@ function paintSeries() {
 
 function favouritesSeries(event) {
   const clickList = parseInt(event.currentTarget.id); //friends o la serie clicada
-  // console.log(clickList);
+  console.log(clickList);
+  let miCoñoSerranoSacaEsteExamen = listSeries[clickList];
+
   const clickFavourite = favouritesList.indexOf(clickList);
   console.log(clickFavourite);
 
   if (clickFavourite === -1) {
-    favouritesList.push(clickList);
+    favouritesList.push(miCoñoSerranoSacaEsteExamen);
     console.log('lo meto');
     // favouritesList.classList.add('js-list-favourite');
   } else {
-    favouritesList.splice(clickFavourite, 1);
+    favouritesList.splice(listSeries, 1);
     console.log('lo quito');
     //   favouritesList.splice(clickFavourite, 1);
   }
   // // console.log(favouritesList.splice(clickFavourite, 1));
-
+  console.log(clickList);
   paintSeries();
   listenList();
+  paintFavList();
 }
-
-// let resultsFav = '';
-// for (let i = 0; i < favouritesList.length; i++) {
-//   resultsFav += `<li  class="js-result-fav js-list-color" id="${i}">`;
-//   resultsFav += `<h2> ${favouritesList[i]} </h2>`;
-//   if (favouritesList[i] === null) {
-//     resultsFav += `<img src = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV."`;
-//   } else {
-//     resultsFav += `<img src = ${favouritesList[i]}
-//       alt = picture serie shown >`;
-//   }
-//   resultsFav += `</li>`;
-// }
-
 function listenList() {
   const listItems = document.querySelectorAll('.js-list-item');
   for (const listItem of listItems) {
@@ -78,6 +71,27 @@ function listenList() {
   }
 }
 
+function paintFavList() {
+  let resultsFav = '';
+  for (let i = 0; i < favouritesList.length; i++) {
+    resultsFav += `<li  class="js-result-fav js-list--random" id="${i}">`;
+    resultsFav += `<h2> ${favouritesList[i].show.name} </h2>`;
+    if (favouritesList[i].show.image === null) {
+      resultsFav += `<img src = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV."`;
+    } else {
+      resultsFav += `<img src = ${favouritesList[i].show.image.medium}
+      alt = picture serie shown  width="100">`;
+    }
+    resultsFav += `</li>`;
+  }
+  console.log(favouritesList);
+  favList.innerHTML = resultsFav;
+}
+const setLocalStorage(){
+
+}
+
+// listenList();
 btn.addEventListener('click', getInfo);
 
 //quitar esta línea
